c19764cc3683fdd65f067d115b8dae6200130ccf
Minhajuddin Khaja <minhajuddin.khaja@instacart.com>
2025-03-29 16:36:33 -0400

    SIDs

--- main_test.go
+++ main_test.go
@@ -8,15 +8,17 @@ import (
 )
 
 const (
-	User    Entity = 1
-	Post    Entity = 2
-	Comment Entity = 3
-	Other   Entity = 4
-	UserV2  Entity = 5
-	UserV3  Entity = 6
+	User      Entity = 1
+	Post      Entity = 2
+	Comment   Entity = 3
+	Other     Entity = 4
+	UserV2    Entity = 5
+	UserV3    Entity = 6
+	SessionID Entity = 7
 )
 
 var prefixer = NewRegistry([]PrefixInfo{
+	{SessionID, "sid"},
 	{User, "user"},
 	{UserV2, "user_v2"},
 	{UserV3, "user_v3"},
@@ -35,8 +37,14 @@ func TestPrefixes(t *testing.T) {
 	assert.NilError(t, err)
 	assert.Equal(t, uuid.String(), "0195e37b-f93f-7518-a9ac-a2be68463c7e")
 
+	// Shorter than hex uuid and urlsafe base64
+	assert.Equal(t, len(prefixer.Serialize(User, uuid)), 27) // user.AZXje_k_dRiprKK-aEY8fg
+	assert.Equal(t, len(uuid.String()), 36)                  // 0195e37b-f93f-7518-a9ac-a2be68463c7e
+
 	e, uuid, err := prefixer.DeserializeWithEntity("post.AZXje_k_dRiprKK-aEY8fg")
 	assert.NilError(t, err)
 	assert.Equal(t, e, Post)
 	assert.Equal(t, uuid.String(), "0195e37b-f93f-7518-a9ac-a2be68463c7e")
+
+	assert.Equal(t, prefixer.Serialize(SessionID, uuid), "sid.AZXje_k_dRiprKK-aEY8fg")
 }
